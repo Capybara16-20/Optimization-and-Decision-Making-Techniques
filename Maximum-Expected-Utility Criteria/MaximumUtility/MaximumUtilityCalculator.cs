a72c2d60b7f03afaf3429439c5c8b5e5652058f3
Dmitry Sharonov <capybara20.07@mail.ru>
2021-10-29 14:08:34 +0300

    Add Maximum-Expected-Utility criteria

---
+++ Maximum-Expected-Utility Criteria/MaximumUtility/MaximumUtilityCalculator.cs
@@ -0,0 +1,209 @@
+﻿using System;
+
+namespace MaximumUtility
+{
+    public class MaximumUtilityCalculator
+    {
+        private double[] probabilityVector;
+        private double[,] utilityMatrix;
+        private int n;
+        private int m;
+
+        public MaximumUtilityCalculator(double[] probabilityVector, double[,] utilityMatrix)
+        {
+            this.probabilityVector = probabilityVector;
+            this.utilityMatrix = utilityMatrix;
+
+            n = probabilityVector.Length;
+            m = utilityMatrix.GetLength(0);
+        }
+
+        private double[] GetProbabilityUtilities()
+        {
+            double[] probabilityUtilities = new double[m];
+            for (int i = 0; i < m; i++)
+            {
+                double probabilityUtility = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    probabilityUtility += utilityMatrix[i, j] * probabilityVector[j];
+                }
+                probabilityUtilities[i] = probabilityUtility;
+            }
+
+            return probabilityUtilities;
+        }
+
+        public void ShowDecision()
+        {
+            int[] decisions = GetDecisions();
+            double[] probabilityUtilities = GetProbabilityUtilities();
+
+            Console.WriteLine("Оптимальные решения:");
+
+            PrintLine(17, 15, n + 1);
+
+            Console.Write("|{0,-17}", "");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("|{0} {1,-5}", "Состояние", i + 1);
+            }
+
+            Console.Write("|{0,-15}", "Выигрыш");
+            Console.WriteLine("|");
+
+            PrintLine(17, 15, n + 1);
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < decisions.Length; j++)
+                {
+                    if (i == decisions[j])
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                }
+
+                Console.Write("|{0} {1,-5}", "Алтернатива", i + 1);
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("|{0,-15:0.###}", utilityMatrix[i, j]);
+                }
+
+                Console.Write("|{0,-15:0.###}", probabilityUtilities[i]);
+                Console.WriteLine("|");
+
+                Console.ResetColor();
+
+                PrintLine(17, 15, n + 1);
+            }
+
+            Console.WriteLine();
+        }
+
+        private int[] GetDecisions()
+        {
+            double[] probabilityUtilities = GetProbabilityUtilities();
+            double max = 0;
+            for (int i = 0; i < m; i++)
+            {
+                if (probabilityUtilities[i] > max)
+                {
+                    max = probabilityUtilities[i];
+                }
+            }
+
+            int count = 0;
+            for (int i = 0; i < m; i++)
+            {
+                if (probabilityUtilities[i] == max)
+                {
+                    count++;
+                }
+            }
+
+            int[] decisions = new int[count];
+            for (int i = 0; i < m; i++)
+            {
+                int index = 0;
+                if (probabilityUtilities[i] == max)
+                {
+                    decisions[index] = i;
+                    index++;
+                }
+            }
+
+            return decisions;
+        }
+
+        public void ShowCalculation()
+        {
+            double[] probabilityUtilities = GetProbabilityUtilities();
+
+            Console.WriteLine("Расчёт ожидаемой полезности альтернатив:");
+            for (int i = 0; i < m; i++)
+            {
+                Console.Write("Полезность {0}-й альтернативы: ", i + 1);
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("{0:0.###} * {1:0.###}", utilityMatrix[i, j], probabilityVector[j]);
+                    if (j != n - 1)
+                    {
+                        Console.Write(" + ");
+                    }
+                }
+                Console.WriteLine(" = {0:0.###}", probabilityUtilities[i]);
+            }
+            Console.WriteLine();
+        }
+
+        public void ShowProbabilities()
+        {
+            Console.WriteLine("Вероятности состояний природы:");
+
+            PrintLine(17, 15, n);
+
+            Console.Write("|{0,-17}", "");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("|{0} {1,-5}", "Состояние", i + 1);
+            }
+            Console.WriteLine("|");
+
+            PrintLine(17, 15, n);
+
+            Console.Write("|{0,-17}", "Вероятность");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("|{0,-15:0.###}", probabilityVector[i]);
+            }
+            Console.WriteLine("|");
+            PrintLine(17, 15, n);
+
+            Console.WriteLine();
+        }
+
+        public void ShowUtilityMatrix()
+        {
+            Console.WriteLine("Матрица полезности:");
+
+            PrintLine(17, 15, n);
+
+            Console.Write("|{0,-17}", "");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("|{0} {1,-5}", "Состояние", i + 1);
+            }
+            Console.WriteLine("|");
+
+            PrintLine(17, 15, n);
+
+            for (int i = 0; i < m; i++)
+            {
+                Console.Write("|{0} {1,-5}", "Алтернатива", i + 1);
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("|{0,-15:0.###}", utilityMatrix[i, j]);
+                }
+                Console.WriteLine("|");
+
+                PrintLine(17, 15, n);
+            }
+
+            Console.WriteLine();
+        }
+
+        private void PrintLine(int rowTitleLength, int colTitleLength, int colCount)
+        {
+            Console.Write("+");
+            Console.Write(new string('-', rowTitleLength));
+            Console.Write("+");
+            for (int i = 0; i < colCount; i++)
+            {
+                Console.Write(new string('-', colTitleLength));
+                Console.Write("+");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Maximum-Expected-Utility Criteria/MaximumUtilityUI/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using MaximumUtility;
+
+namespace MaximumUtilityUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = InputInteger("Введите количество состояний природы: ", "Введите целое число больше 0!");
+            double[] probabilityVector = InputProbabilityVector(n, "Введите вероятности состояний природы:");
+            int m = InputInteger("Введите количество альтернатив: ", "Введите целое число больше 0!");
+            double[,] utilityMatrix = InputUtilityMatrix(n, m, "Введите матрицу полезности:");
+            Console.Clear();
+
+            MaximumUtilityCalculator calculator = new MaximumUtilityCalculator(probabilityVector, utilityMatrix);
+            calculator.ShowProbabilities();
+            calculator.ShowUtilityMatrix();
+            calculator.ShowCalculation();
+            calculator.ShowDecision();
+        }
+
+        private static double[] InputProbabilityVector(int n, string inputMessage)
+        {
+            const string failureMessage = "Введите число в диапазоне [0; 1]!";
+
+            Console.WriteLine(inputMessage);
+
+            double[] vector = new double[n];
+            bool isProbabilitiesCorrect = false;
+            while (!isProbabilitiesCorrect)
+            {
+                double sum = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    vector[i] = InputDouble(string.Format("Состояние {0}: ", i + 1), failureMessage, true, 1);
+                    sum = Math.Round(sum + vector[i], 1);
+                }
+
+                if (sum == 1)
+                    isProbabilitiesCorrect = true;
+                else
+                    Console.WriteLine("Сумма вероятностей должна быть равна 1!");
+            }
+
+            return vector;
+        }
+
+        private static double[,] InputUtilityMatrix(int n, int m, string inputMessage)
+        {
+            const string failureMessage = "Введите число!";
+
+            Console.WriteLine(inputMessage);
+
+            double[,] matrix = new double[m, n];
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++)
+                    matrix[i, j] = InputDouble(string.Format("[{0}, {1}]: ", i + 1, j + 1), failureMessage, false);
+
+            Console.WriteLine();
+
+            return matrix;
+        }
+
+        private static int InputInteger(string inputMessage, string failureMessage)
+        {
+            int x = 0;
+            bool isCorrect = false;
+            while (!isCorrect)
+            {
+                Console.Write(inputMessage);
+
+                if (int.TryParse(Console.ReadLine(), out x))
+                    if (x > 0)
+                        isCorrect = true;
+
+                if (!isCorrect)
+                    Console.WriteLine(failureMessage);
+            }
+
+            return x;
+        }
+
+        private static double InputDouble(string inputMessage, string failureMessage, bool isPositiv, int? maxValue = null)
+        {
+            double x = 0;
+            bool isCorrect = false;
+            while (!isCorrect)
+            {
+                Console.Write(inputMessage);
+
+                if (double.TryParse(Console.ReadLine(), out x))
+                {
+                    if (isPositiv)
+                    {
+                        if (x >= 0)
+                        {
+                            if (maxValue.HasValue)
+                            {
+                                if (x <= maxValue)
+                                {
+                                    isCorrect = true;
+                                }
+                            }
+                            else
+                            {
+                                isCorrect = true;
+                            }
+                            
+                        }
+                    }
+                    else
+                    {
+                        if (maxValue.HasValue)
+                        {
+                            if (x <= maxValue)
+                            {
+                                isCorrect = true;
+                            }
+                        }
+                        else
+                        {
+                            isCorrect = true;
+                        }
+                    }
+                }
+
+                if (!isCorrect)
+                {
+                    Console.WriteLine(failureMessage);
+                }
+            }
+
+            return x;
+        }
+    }
+}

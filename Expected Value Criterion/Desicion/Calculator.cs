a3f07eb484d0fe6b3f9432eae3237b24b3bf5e4a
Dmitry Sharonov <capybara20.07@mail.ru>
2021-10-29 15:35:03 +0300

    Add expected value criterion

---
+++ Expected Value Criterion/Desicion/Calculator.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace Desicion
+{
+    public class Calculator
+    {
+        readonly double[] probabilities;
+        readonly double[,] utilityMatrix;
+
+        public Calculator(double[] probabilities, double[,] utilityMatrix)
+        {
+            this.probabilities = probabilities;
+            this.utilityMatrix = utilityMatrix;
+        }
+
+        double[,] UtilityProbabilitiesMatrix
+        {
+            get
+            {
+                double[,] utilityProbabilitiesMatrix = new double[utilityMatrix.GetLength(0), utilityMatrix.GetLength(1)];
+                for (int j = 0; j < utilityProbabilitiesMatrix.GetLength(1); j++)
+                    for (int i = 0; i < utilityProbabilitiesMatrix.GetLength(0); i++)
+                        utilityProbabilitiesMatrix[i, j] = utilityMatrix[i, j] * probabilities[j];
+
+                return utilityProbabilitiesMatrix;
+            }
+        }
+
+        double[] ExpectedValues
+        {
+            get
+            {
+                double[] expectedValues = new double[UtilityProbabilitiesMatrix.GetLength(0)];
+                for (int i = 0; i < UtilityProbabilitiesMatrix.GetLength(0); i++)
+                    for (int j = 0; j < UtilityProbabilitiesMatrix.GetLength(1); j++)
+                        expectedValues[i] += UtilityProbabilitiesMatrix[i, j];
+
+                return expectedValues;
+            }
+        }
+
+        public void ShowProbabilities()
+        {
+            for (int i = 0; i < probabilities.Length; i++)
+            {
+                Console.Write("{0,-10}|", "Состояние" + (i + 1));
+                Console.Write("{0,-6:0.###}", probabilities[i]);
+                Console.WriteLine();
+            }
+        }
+
+        public void ShowUtilityMatrix()
+        {
+            ShowMatrix(utilityMatrix);
+        }
+
+        public void ShowUtilityProbabilitiesMatrix()
+        {
+            ShowMatrix(UtilityProbabilitiesMatrix);
+        }
+
+        private void ShowMatrix(double[,] matrix)
+        {
+            Console.Write("{0,-13}|", "");
+            for (int i = 0; i < matrix.GetLength(1); i++)
+                Console.Write("{0,-10}|", "Состояние" + (i + 1));
+            Console.WriteLine();
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.Write("{0,-13}|", "Альтернатива" + (i + 1));
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    Console.Write("{0,-10:0.###}|", matrix[i, j]);
+                Console.WriteLine();
+            }
+        }
+
+        public void ShowExpectedValues()
+        {
+            for (int i = 0; i < ExpectedValues.GetLength(0); i++)
+            {
+                Console.Write("{0,-13}|", "Альтернатива" + (i + 1));
+                Console.Write("{0,-6:0.###}", ExpectedValues[i]);
+                Console.WriteLine();
+            }
+        }
+
+        public void ShowDecision()
+        {
+            int[] decision = GetDesicion();
+            
+            Console.Write("Оптимальное(ые) решение: {0}", decision[0]);
+            for (int i = 0; i < decision.Length; i++)
+                if (i > 0)
+                    Console.Write(", {0}", decision[i]);
+            Console.WriteLine();
+        }
+
+        private int[] GetDesicion()
+        {
+            double maxValue = 0;
+            int count = 0;
+            for (int i = 0; i < ExpectedValues.Length; i++)
+            {
+                if (ExpectedValues[i] > maxValue)
+                {
+                    count = 1;
+                    maxValue = ExpectedValues[i];
+                }
+                else if (ExpectedValues[i] == maxValue)
+                    count++;
+            }
+
+            int[] decision = new int[count];
+            int index = 0;
+            for (int i = 0; i < ExpectedValues.Length; i++)
+                if (ExpectedValues[i] == maxValue)
+                {
+                    decision[index] = i + 1;
+                    index++;
+                }
+
+            return decision;
+        }
+    }
+}
---
+++ Expected Value Criterion/DesicionUI/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Desicion;
+
+namespace DesicionUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество состояний природы: ");
+            int n = InputCount();
+
+            Console.WriteLine("Введите вероятности состояний природы:");
+            double[] probabilities = GetProbabilities(n);
+
+            Console.Write("Введите количество альтернатив: ");
+            int m = InputCount();
+
+            Console.WriteLine("Введите матрицу полезности:");
+            double[,] utilityMatrix = new double[m, n];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("Альтернатива {0} Состояние {1}: ", i + 1, j + 1);
+                    utilityMatrix[i, j] = InputValue(0);
+                }
+            }
+
+            /*double[] probabilities = new double[] { 0.3, 0.5, 0.2 };
+            double[,] utilityMatrix = new double[,] { { 3, 16, 5 }, { 6, 15, 8 }, { 6, 9, 4 }, { 6, 15, 8 } };*/
+            
+            Calculator calculator = new Calculator(probabilities, utilityMatrix);
+
+            Console.WriteLine();
+            Console.WriteLine("Вероятности:");
+            calculator.ShowProbabilities();
+            Console.WriteLine();
+
+            Console.WriteLine("Матрица полезности:");
+            calculator.ShowUtilityMatrix();
+            Console.WriteLine();
+
+            Console.WriteLine("Матрица вероятностей полезности:");
+            calculator.ShowUtilityProbabilitiesMatrix();
+            Console.WriteLine();
+
+            Console.WriteLine("Математическое ожидание:");
+            calculator.ShowExpectedValues();
+            Console.WriteLine();
+
+            calculator.ShowDecision();
+
+            Console.ReadLine();
+        }
+
+        private static double[] GetProbabilities(int n)
+        {
+            double[] probabilities = new double[n];
+            bool isCorrect = false;
+            while (!isCorrect)
+            {
+                double sum = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write("Вероятность {0}: ", i + 1);
+                    probabilities[i] = InputValue(0, 1);
+                    sum += probabilities[i];
+                }
+
+                if (sum == 1)
+                    isCorrect = true;
+                else
+                    Console.WriteLine("Сумма вероятностей должна равняться 1");
+            }
+
+            return probabilities;
+        }
+
+        private static int InputCount()
+        {
+            int value = 0;
+            bool isCorrect = false;
+            while (!isCorrect)
+            {
+                if (int.TryParse(Console.ReadLine(), out value))
+                    if (value > 0)
+                        isCorrect = true;
+
+                if (!isCorrect)
+                    Console.Write("Введите целое число больше 0: ");
+            }
+
+            return value;
+        }
+
+        private static double InputValue(int minBorder, int? maxBorder = null)
+        {
+            double value = 0;
+            bool isCorrect = false;
+            while (!isCorrect)
+            {
+                if (double.TryParse(Console.ReadLine(), out value))
+                    if ((value > minBorder) && ((maxBorder.HasValue && value < maxBorder) || !maxBorder.HasValue))
+                        isCorrect = true;
+
+                if (!isCorrect)
+                    if (maxBorder.HasValue)
+                        Console.Write("Введите вещественное число больше {0} меньше {1}: ", minBorder, maxBorder);
+                    else
+                        Console.Write("Введите вещественное число больше {0}: ", minBorder);
+            }
+
+            return value;
+        }
+    }
+}

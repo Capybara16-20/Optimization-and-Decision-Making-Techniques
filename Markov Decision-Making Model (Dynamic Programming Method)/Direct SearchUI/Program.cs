bfca1fb11ace159b53766be55cf62508d686c110
Dmitry Sharonov <capybara20.07@mail.ru>
2021-10-29 14:09:37 +0300

    Add dynamic programming method

---
+++ Markov Decision-Making Model (Dynamic Programming Method)/Direct SearchUI/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using DirectSearch;
+
+namespace DirectSearchUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = InputInteger("Введите количество стратегий: ", "Должно быть целое положительное число.");
+            Console.WriteLine();
+
+            double[,,] probabilityMatrices = new double[n, n, n];
+            int[,,] payoffMatrices = new int[n, n, n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Стратегия {0}:", i + 1);
+                double[,] probabilityMatrix = InputProbabilityMatrix(n, "Введите матрицу вероятностей:");
+                for (int j = 0; j < n; j++)
+                {
+                    for (int z = 0; z < n; z++)
+                    {
+                        probabilityMatrices[i, j, z] = probabilityMatrix[j, z];
+                    }
+                }
+
+                int[,] payoffMatrix = InputPayoffMatrix(n, "Введите матрицу выигрышей:");
+                for (int j = 0; j < n; j++)
+                {
+                    for (int z = 0; z < n; z++)
+                    {
+                        payoffMatrices[i, j, z] = payoffMatrix[j, z];
+                    }
+                }
+            }
+            Console.Clear();
+
+
+            /*double[,,] probabilityMatrices = { { { 0.4, 0.45, 0.15 }, { 0.1, 0.6, 0.3 }, { 0.1, 0.4, 0.5 } },
+                                               { { 0.7, 0.15, 0.15 }, { 0.25, 0.6, 0.15 }, { 0.1, 0.7, 0.2} },
+                                               { { 0.2, 0.6, 0.2 }, { 0, 0.8, 0.2 }, { 0, 0.3, 0.7} } };
+
+            int[,,] payoffMatrices = { { { 400, 520, 650 }, { 350, 420, 700 }, { 200, 300, 500 } },
+                                       { { 900, 1100, 1700 }, { 350, 950, 1750 }, { 550, 750, 1150} },
+                                       { { 400, 550, 700 }, { 370, 480, 1750 }, { 270, 450, 1150} } };
+
+            DirectSearchProcessor processor = new DirectSearchProcessor(probabilityMatrices, payoffMatrices, 3);*/
+
+            DirectSearchProcessor processor = new DirectSearchProcessor(probabilityMatrices, payoffMatrices, n);
+             processor.Calculate();
+            
+
+        }
+
+        private static double[,] InputProbabilityMatrix(int n, string inputMessage)
+        {
+            const string failureMessage = "Должно быть вещественное неотрицательное число.";
+
+            Console.WriteLine(inputMessage);
+
+            double[,] matrix = new double[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                bool isProbabilitiesCorrect = false;
+                while (!isProbabilitiesCorrect)
+                {
+                    double sum = 0;
+                    for (int j = 0; j < n; j++)
+                    {
+                        matrix[i, j] = InputDouble(string.Format("[{0}, {1}]: ", i + 1, j + 1), failureMessage);
+                        sum = Math.Round(sum + matrix[i, j], 1);
+                    }
+
+                    if (sum == 1)
+                        isProbabilitiesCorrect = true;
+                    else
+                        Console.WriteLine("Сумма вероятностей должна равняться 1.");
+                }
+            }
+
+            return matrix;
+        }
+
+        private static int[,] InputPayoffMatrix(int n, string inputMessage)
+        {
+            const string failureMessage = "Должно быть целое положительное число.";
+
+            Console.WriteLine(inputMessage);
+
+            int[,] matrix = new int[n, n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    matrix[i, j] = InputInteger(string.Format("[{0}, {1}]: ", i + 1, j + 1), failureMessage);
+
+            Console.WriteLine();
+
+            return matrix;
+        }
+
+        private static double InputDouble(string inputMessage, string failureMessage)
+        {
+            double x = 0;
+            bool isCorrect = false;
+            while (!isCorrect)
+            {
+                Console.Write(inputMessage);
+
+                if (double.TryParse(Console.ReadLine(), out x))
+                    if (x >= 0)
+                        isCorrect = true;
+
+                if (!isCorrect)
+                    Console.WriteLine(failureMessage);
+            }
+
+            return x;
+        }
+
+        private static int InputInteger(string inputMessage, string failureMessage)
+        {
+            int x = 0;
+            bool isCorrect = false;
+            while (!isCorrect)
+            {
+                Console.Write(inputMessage);
+
+                if (int.TryParse(Console.ReadLine(), out x))
+                    if (x > 0)
+                        isCorrect = true;
+
+                if (!isCorrect)
+                    Console.WriteLine(failureMessage);
+            }
+
+            return x;
+        }
+    }
+}
---
+++ Markov Decision-Making Model (Dynamic Programming Method)/DirectSearch/DirectSearchProcessor.cs
@@ -0,0 +1,149 @@
+﻿using System;
+
+namespace DirectSearch
+{
+    public class DirectSearchProcessor
+    {
+        public int N { get; }
+        public double[,,] ProbabilityMatrices { get; private set; }
+        public int[,,] PayoffMatrix { get; private set; }
+
+
+        public DirectSearchProcessor(double[,,] probabilityMatrices, int[,,] payoffMatrices, int n)
+        {
+            ProbabilityMatrices = probabilityMatrices;
+            PayoffMatrix = payoffMatrices;
+            N = n;
+        }
+
+        public void Calculate()
+        {
+            double[] stepCosts = new double[N];
+            double[,] probabilityCosts = GetProbabilityCosts(ProbabilityMatrices, PayoffMatrix);
+            for (int i = 0; i < N; i++)
+            {
+                double[,] probabilityMatrix = new double[N, N];
+                int[,] payoffMatrix = new int[N, N];
+                for (int j = 0; j < N; j++)
+                {
+                    for (int k = 0; k < N; k++)
+                    {
+                        probabilityMatrix[j, k] = ProbabilityMatrices[i, j, k];
+                        payoffMatrix[j, k] = PayoffMatrix[i, j, k];
+                    }
+                }
+
+                double[,] stepProbabilityCosts = GetStepProbabilityCosts(probabilityCosts, stepCosts);
+                
+                stepCosts = GetStepCosts(stepProbabilityCosts);
+
+                int[] decision = GetDecision(stepProbabilityCosts, stepCosts);
+
+                ShowStepResults(stepProbabilityCosts, stepCosts, decision, i + 1);
+            }
+
+        }
+
+        private double[,] GetProbabilityCosts(double[,,] probabilityMatrices, int[,,] payoffMatrices)
+        {
+            double[,] probabilityCosts = new double[N, N];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    for (int k = 0; k < N; k++)
+                    {
+                        probabilityCosts[j, i] += probabilityMatrices[i, j, k] * payoffMatrices[i, j, k];
+                    }
+                }
+            }
+
+            return probabilityCosts;
+        }
+
+        private double[,] GetStepProbabilityCosts(double[,] probabilityCosts, double[] stepCosts)
+        {
+            double[,] stepProbabilityCosts = new double[N, N];
+            for (int j = 0; j < N; j++)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    double a = 0;
+                    for (int k = 0; k < N; k++)
+                    {
+                        a += stepCosts[k] * ProbabilityMatrices[j, i, k];
+                    }
+                    stepProbabilityCosts[i, j] = probabilityCosts[i, j] + a;
+                }
+            }
+
+            return stepProbabilityCosts;
+        }
+
+        private double[] GetStepCosts(double[,] probabilityCosts)
+        {
+            double[] stepCosts = new double[N];
+            for (int i = 0; i < N; i++)
+            {
+                double max = 0;
+                for (int j = 0; j < N; j++)
+                {
+                    if (probabilityCosts[i, j] > max)
+                    {
+                        max = probabilityCosts[i, j];
+                    }
+                }
+
+                stepCosts[i] = max;
+            }
+
+            return stepCosts;
+        }
+
+        private int[] GetDecision(double[,] stepProbabilityCosts, double[] stepCosts)
+        {
+            int[] decision = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (stepProbabilityCosts[i, j] == stepCosts[i])
+                    {
+                        decision[i] = j + 1;
+                    }
+                }
+            }
+
+            return decision;
+        }
+
+        private void ShowStepResults(double[,] stepProbabilityCosts, double[] stepCosts, int[] decision, int step)
+        {
+
+            Console.WriteLine("Результат выполнения {0}-го шага:", step);
+
+            Console.Write("{0,-15}", "Состояние");
+            for (int i = 0; i < N; i++)
+            {
+                
+                Console.Write("{0} {1,-5}", "Стратегия", i + 1);
+            }
+            Console.Write("{0,-15}", "F");
+            Console.Write("{0,-15}", "Z");
+            Console.WriteLine();
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write("{0,-15}", i + 1);
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write("{0,-15:0.###}", stepProbabilityCosts[i, j]);
+                }
+                Console.Write("{0,-15:0.###}", stepCosts[i]);
+                Console.Write("{0,-15}", decision[i]);
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
